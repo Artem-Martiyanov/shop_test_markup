11c5b7eb722ad7df2716b0d357a779dcbd7f4d76
Artem-Martiyanov <artem-martiyanov@mail.ru>
2023-02-16 22:47:26 +0300

    Дорабатывается версия для ПК

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './styles/app.css'
 import Slider from "./components/Slider/Slider";
-import product from "./data/product";
+import product from "./product";
 import Button from "./components/UI/button/Button";
 import Select from "./components/UI/select/Select";
 import LikeButton from "./components/UI/likeButton/LikeButton";
@@ -11,25 +11,29 @@ import BreadCrumbs from "./components/UI/breadCrumbs/BreadCrumbs";
 
 
 function App() {
+    const splitDigits = (price) => price.toString().replace(/\d{1,3}(?=(\d{3})+(?!\d))/g, '$& ')
 
     return (
         <div className="App">
             <BreadCrumbs/>
-            <Features/>
-            <Slider images={product.images}/>
             <header className='product__header'>
-                <h1 className='product__title'>{product.name}</h1>
-                <span className='product__price'>{product.price} руб.</span>
+                <div className='product__top'>
+                    <h1 className='product__title'>{product.name}</h1>
+                    <ExitBtn/>
+                </div>
+                <span className='product__price'>{splitDigits(product.price)} руб.</span>
+
             </header>
-            <ExitBtn/>
             <section className='product__interactive'>
                 <Select options={product.sizes} name={'size'}/>
                 <Button>В корзину</Button>
+                <LikeButton/>
                 <SizeList sizes={product.sizes}/>
                 <a href='#' className='product__link'>Как выбрать размер?</a>
             </section>
-            <LikeButton/>
-            <p className='product__description'>{product.description}</p>
+            <Slider images={product.images}/>
+            <div className='product__description'>{product.description}</div>
+            <Features features={product.features}/>
         </div>
     );
 }
