45f283bf466ac1a9daa4bdf610d2cbf0099f305e
Artem-Martiyanov <artem-martiyanov@mail.ru>
2023-02-20 19:29:51 +0300

    Добавялет логику хлебным крошкам

--- src/App.js
+++ src/App.js
@@ -1,44 +1,20 @@
 import './styles/app.css'
-import Slider from "./components/Slider/Slider";
+import {Routes, Route} from "react-router-dom";
+import ProductPage from "./pages/ProductPage/ProductPage";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import HowChoose from "./pages/HowChoose";
 import product from "./product";
-import Button from "./components/UI/button/Button";
-import Select from "./components/UI/select/Select";
-import LikeButton from "./components/UI/likeButton/LikeButton";
-import SizeList from "./components/UI/sizeList/SizeList";
-import Features from "./components/UI/features/Features";
-import ExitBtn from "./components/UI/exitBtn/ExitBtn";
-import BreadCrumbs from "./components/UI/breadCrumbs/BreadCrumbs";
-
 
 function App() {
-    const splitDigits = (price) => price.toString().replace(/\d{1,3}(?=(\d{3})+(?!\d))/g, '$& ')
-
     return (
         <div className="App">
-            <BreadCrumbs/>
-            <header className='product__header'>
-                <div className='product__top'>
-                    <h1 className='product__title'>{product.name}</h1>
-                    <ExitBtn/>
-                </div>
-                <span className='product__price'>{splitDigits(product.price)} руб.</span>
-            </header>
-            <section className='product__interactive'>
-                <div className='product__buttons'>
-                    <Select options={product.sizes} name={'size'}/>
-                    <div className='bin__buttons'>
-                        <Button>В корзину</Button>
-                        <LikeButton/>
-                    </div>
-                </div>
-                <div className='product__size-list'>
-                    <SizeList sizes={product.sizes}/>
-                    <a href='#' className='product__link'>Как выбрать размер?</a>
-                </div>
-            </section>
-            <Slider images={product.images}/>
-            <div className='product__description'>{product.description}</div>
-            <Features features={product.features}/>
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/product" element={<ProductPage product={product}/>}/>
+                <Route path="/product/howtochoose" element={<HowChoose/>}/>
+                <Route path="*" element={<NotFoundPage/>}/>
+            </Routes>
         </div>
     );
 }
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,1 @@
+import React from 'react';import {Link} from "react-router-dom";const NotFoundPage = () => {    return (        <>            <h1>Страница не найдена</h1>            <Link to='/'>На главную</Link>        </>    )};export default NotFoundPage;
